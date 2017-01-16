584b9eb6a2af0b2985bb0784bed8fcba87345ea1
Reza Motevallizadeh <rezamt@gmail.com>
2017-01-16 12:12:59 +1100

    my work on tutorial 1

--- src/main/java/com/stormpath/tutorial/config/SpringSecurityWebAppConfig.java
+++ src/main/java/com/stormpath/tutorial/config/SpringSecurityWebAppConfig.java
@@ -27,8 +27,8 @@ public class SpringSecurityWebAppConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .apply(stormpath()).and()
-            .authorizeRequests()
-            .antMatchers("/").permitAll();
+                .apply(stormpath()).and()
+                .authorizeRequests()
+                .antMatchers("/").permitAll();
     }
 }
